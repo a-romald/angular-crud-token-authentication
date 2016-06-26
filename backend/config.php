0831840b4d9cd53c1b3ed1ec6055c62dc5f52e19
Roma <a_rhoma@mail.ru>
2016-06-26 12:05:23 +0400

    second fix commit

--- backend/config.php
+++ backend/config.php
@@ -3,6 +3,6 @@
     SQL settings
   */
   define('MYSQL_SERVER', 'localhost');
-  define('MYSQL_USER', 'root');
-  define('MYSQL_PASSWORD', '');
+  define('MYSQL_USER', 'user');
+  define('MYSQL_PASSWORD', 'password');
   define('MYSQL_DB', 'angular');
